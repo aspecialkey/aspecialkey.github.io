3a899e0750404bfd661ff7be5489bab916f58c87
aspecialkey <k.kober@posteo.de>
2019-10-27 13:44:18 +0100

    updated paths

--- ea1/js/script.js
+++ ea1/js/script.js
@@ -8,15 +8,12 @@ var firePos = 0;
 
 window.onload = function () {
 
-
     cacheImages();
     updateImgElement();
 
     setInterval(function () {
         if (autoRotate) rotateImage(direction);
     }, 130);
-
-    // document.getElementById("textDiv").style.visibility = "visible";
 
     document.getElementsByTagName("div")[0].style.visibility = "visible";
     document.getElementsByTagName("div")[1].style.visibility = "visible";
@@ -27,7 +24,6 @@ window.onload = function () {
 
 window.onkeydown = function (evt) {
 
-    console.log("Key: "+  evt.code);
     switch (evt.code) {
         case ('KeyR'):
             // console.log("rechts");
@@ -40,9 +36,8 @@ window.onkeydown = function (evt) {
             rotateImage(direction);
             break;
         case ('Space'):
-            autoRotate = autoRotate ? false : true;
+            autoRotate = !autoRotate;
     }
-
 
 };
 
@@ -53,7 +48,7 @@ function cacheImages() {
 
     for (i = 0; i < 360; i += ANGLESTEP) {
         let img = new Image();
-        img.src = "/ea1/img/spirale" + i +".png";
+        img.src = "img/spirale" + i +".png";
     }
 }
 
@@ -77,16 +72,15 @@ function updateImgElement() {
     let imgHeight = window.innerWidth < window.innerHeight ? window.innerWidth : window.innerHeight;
     let img = document.getElementById("spiraleImage");
     img.width = imgHeight;
-    img.height =imgHeight;
+    img.height = imgHeight;
 
-    img.src = "/ea1/img/spirale" + angle +".png";
+    img.src = "img/spirale" + angle +".png";
 }
 
 function animateFire() {
     setInterval(function () {
         let img = document.getElementById("feuerImage");
-        img.src = "/ea1/img/feuer" + firePos++ +".png";
+        img.src = "img/feuer" + firePos++ +".png";
         if(firePos === 18) firePos = 0;
-
     }, 150);
 }
